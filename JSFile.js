85eae9c47ecd6acd1a28582e0aa302e689e955cc
dianmiao <dianmiao@sohu-inc.com>
2013-08-01 19:14:46 +0800

    ud

--- JSFile.js
+++ JSFile.js
@@ -22,14 +22,16 @@
         Blob = win.Blob,
         // 流数据类型
         forceAaveableType = 'application/octet-stream',
-        event = {},
+        Event = {},
         eventObj = {},
         specialEvents = {},
         uuid = 1,
         getUid = function(obj) {
             return obj.uniqueNumber || (obj.uniqueNumber = uuid++);
         },
-        rword = /[^, ]+/g;
+        rword = /[^, ]+/g,
+        noop = function() {},
+        Upload;
 
     function getType(obj, typeVal) {
         var ret = '';
@@ -95,56 +97,78 @@
         }
     }
 
+    if (!each.bind) {
+        Function.prototype.bind = function(context) {
+            var that = this;
+            var args = Array.prototype.slice.call(arguments, 1);
+            return function() {
+                var ars = args.concat();
+                ars.push.apply(ars, arguments)
+                return that.apply(context, ars);
+            };
+        };
+    }
+
     specialEvents.click = specialEvents.mousedown =
     specialEvents.mouseup = specialEvents.mousemove = 'MouseEvents';
 
-    mix(event, {
+    mix(Event, {
 
         on: function(ele, name, func, bubble) {
-            if (ele.addEventListener) {
-                ele.addEventListener(name, func, !!bubble);
-            } else {
-                var uid = getUid(ele);
-                var hd = eventObj[uid] || (eventObj[uid] = {});
-                if (!hd[name]) hd[name] = [];
-                hd[name].push({
-                    func: func,
-                    bubble: !!bubble
-                });
-            }
+            var uid = getUid(ele);
+            var hd = eventObj[uid] || (eventObj[uid] = {});
+            if (!hd[name]) hd[name] = [];
+            bubble = !!bubble;
+            hd[name].push({
+                func: func,
+                bubble: bubble
+            });
+            ele.addEventListener && ele.addEventListener(name, func, bubble);
         },
 
         off: function(ele, name, func, bubble) {
-            if (ele.removeEventListener) {
-                ele.removeEventListener(name, func, !!bubble);
-            } else {
-                var uid = getUid(ele);
-                var hd = eventObj[uid];
-                if (!hd) return;
-                hd = hd[name];
-                if (!hd || !hd.length) return;
+            var uid = getUid(ele);
+            var hd = eventObj[uid];
+            if (!hd) return;
+            hd = hd[name];
+            if (!hd || !hd.length) return;
+            bubble = !!bubble;
+            if (func) {
                 each(hd, function(obj, index) {
-                    if (obj.func === func && obj.bubble === (!!bubble)) {
+                    if (obj.func === func && obj.bubble === (bubble)) {
                         hd.splice(index, 1);
+                        ele.removeEventListener && ele.removeEventListener(name, func, bubble);
                     }
                 });
+            } else {
+                each(hd, function(obj, index) {
+                    ele.removeEventListener && ele.removeEventListener(name, obj.func, obj.bubble);
+                });
+                hd.length = 0;
             }
         },
 
-        trigger: function(ele, name, data) {
+        trigger: function(ele, name, e, data) {
+            if (!data) {
+                data = e;
+            }
             if (ele.dispatchEvent) {
-                var e = doc.createEvent(specialEvents[name] || 'Events');
-                e.type = name;
-                e.data = data;
-                e.target = ele;
-                e.initEvent(name, true, true);
+                if (!e) {
+                    e = doc.createEvent(specialEvents[name] || 'Events');
+                    e.type = name;
+                    e.data = data;
+                    e.target = ele;
+                    e.initEvent(name, true, true);
+                }
+                if (!e.data) e.data = data;
                 ele.dispatchEvent(e);
+                return true;
             } else {
                 var uid = getUid(ele);
                 var hd = eventObj[uid];
-                if (!hd) return;
+                if (!hd) return false;
                 hd = hd[name];
-                if (!hd || !hd.length) return;
+                if (!hd || !hd.length) return false;
                 each(hd, function(obj) {
                     obj.func.call(ele, {
                         type: name,
@@ -158,16 +182,33 @@
     });
     
     // 支持jquery式多事件('click dblclick'|'click,dblclick')绑定
-    each(event, function(func, name) {
-        event[name] = function(ele, names) {
+    each(Event, function(func, name) {
+        Event[name] = function(ele, names) {
+            var args = arguments;
+            if (names === Object(names)) {
+                each(names, function(handler, nms) {
+                    args.callee.call(Event, ele, nms, handler, args[2]);
+                });
+            }
+            if (!getType(names, 'string')) return false;
             names = names.match(rword);
-            if (!names || !names.length) return;
+            if (!names || !names.length) return false;
             var i = names.length;
             do {
-                func.call(event, ele, names[i], arguments[2], arguments[3]);
+                func.call(Event, ele, names[i], args[2], args[3]);
             } while(--i);
         };
     });
+    function handlerFalse(func, context) {
+        var rfalse = {val: false};
+        context || (context = win);
+        func.call(context, rfalse);
+        return !rfalse.val;
+    }
+    Event.stop = function(e) {
+        e.stopPropagation();
+        e.preventDefault();
+    };
 
     // URL 具有 createObjectURL revokeObjectURL两个方法
     // 前者是创建对象URL 后者是释放对象URL
@@ -244,7 +285,7 @@
                     win.open(objURL, '_blank');
                 }
                 filesaver.readyState = filesaver.DONE;
-                event.trigger(filesaver, 'writestart progress write writeend');
+                Event.trigger(filesaver, 'writestart progress write writeend');
             },
             createIfNotFound = {create: true, exclusive: false},
             objURL, targetView;
@@ -254,10 +295,10 @@
             if (canUseSaveLink) {
                 objURL = linkA.href = getObjURL(blob);
                 linkA.download = name;
-                event.trigger(linkA, 'click');
+                Event.trigger(linkA, 'click');
                 click(linkA);
                 filesaver.readyState = filesaver.DONE;
-                event.trigger(filesaver, 'writestart progress write writeend');
+                Event.trigger(filesaver, 'writestart progress write writeend');
                 return;
             }
             // Object and web filesystem URLs have a problem saving in Google Chrome when
@@ -303,7 +344,7 @@
                                     targetView && targetView.location.href = fileEntry.toURL();
                                     deletionQueue.push(fileEntry);
                                     filesaver.readyState = filesaver.DONE;
-                                    event.trigger(filesaver, 'writeend', evt);
+                                    Event.trigger(filesaver, 'writeend', evt);
                                 };
                                 fileWriter.onerror = function(err) {
                                     err = err || fileWriter.error;
@@ -314,7 +355,7 @@
                                 // 通知外界fileSaver触发某些事件
                                 each('writestart progress write abort'.split(' '), function(evtName) {
                                     fileWriter['on' + evtName] = function(evt) {
-                                        event.trigger(filesaver, evtName, evt);
+                                        Event.trigger(filesaver, evtName, evt);
                                     };
                                 });
                                 fileWriter.write(blob);
@@ -355,7 +396,7 @@
 
             abort: function() {
                 this.readyState = this.DONE;
-                event.trigger(this, 'abort');
+                Event.trigger(this, 'abort');
             },
 
             INIT: 0,
@@ -369,7 +410,7 @@
             return new FileSaver(blob, name);
         }
 
-        event.on(win, 'unload', function() {
+        Event.on(win, 'unload', function() {
             // 释放 URL对象以及File对象
             var i = deletionQueue.length;
             while (i--) {
@@ -545,24 +586,253 @@
         };
     }
 
+    Upload = function() {
+
+        var support = {
+
+            xhrFileUpload: !!(win.XMLHttpRequestUpload && win.FileReader),
+
+            xhrFormDataFileUpload: !!window.FormData
+
+        };
+
+        function Upload(options) {
+            this.options = mix({
+
+                dropZone: doc,
+
+                pasteZone: doc,
+
+                fileInput: null,
+
+                singleFileUploads: true,
+
+                limitMultiFileUploads: false,
+
+                sequentialUploads: false,
+
+                limitConcurrentUploads: undefined,
+
+                multipart: true,
+
+                maxChunkSize: undefined,
+
+                uploadedBytes: undefined,
+
+                recalculateProgress: true,
+
+                progressInterval: 100,
+
+                bitrateInterval: 500,
+
+                autoUpload: true,
+
+                messages: {
+                    uploadedBytes: '上传文件过大'
+                },
+
+                i18n: function(message, context) {
+                    message = this.messages[message] || message.toString();
+                    if (context) {
+                        each(context, function (value, key) {
+                            message = message.replace('{' + key + '}', value);
+                        });
+                    }
+                    return message;
+                },
+
+                formData: function(form) {
+                    return form.serializeArray();
+                },
+
+                add: function(e, data) {
+
+                },
+
+                processData: false,
+
+                contentType: false,
+
+                cache: false,
+
+                onDragEnter: noop,
+
+                onDragOver: noop,
+
+                onDrop: noop,
+
+                onPaste: noop,
+
+                onChange: noop
+
+            }, options);
+    
+            this.init();
+
+        }
+
+        mix(Upload.prototype, {
+
+            init: function() {
+                this.
+                this._initEvents();
+            },
+
+            _initEvents: function() {
+                if (this._isXHRUpload(this.options)) {
+                    Event.on(this.options.dropZone, {
+                        'dragenter': this._onDragEnter.bind(this),
+                        'dragover': this._onDragOver.bind(this),
+                        'drop': this._onDrop.bind(this)
+                    });
+                    Event.on(this.options.pasteZone, 'paste', this._onPaste.bind(this));
+                }
+                this.options.fileInput &&
+                    Event.on(this.options.fileInput, 'change', this._onChange.bind(this));
+            },
+
+            _destroyEvents: function() {
+                Event.off(this.options.dropZone, 'dragenter dragover drop');
+                Event.off(this.options.pasteZone, 'paste');
+                this.options.fileInput &&
+                    Event.off(this.options.fileInput, 'change');
+            },
+
+            _onDragEnter: function(e) {
+                Event.stop(e);
+                this.options.onDragEnter.call(this, e);
+            },
+
+            _onDragOver: function(e) {
+                Event.stop(e);
+                this.options.onDragOver.call(this, e);
+            },
+
+            _onDrop: function(e) {
+                Event.stop(e);
+                var dt = e.dataTransfer;
+                var files = dt.files;
+                // handleFiles(files);
+                this.options.onDrop.call(this, e);
+            },
+
+            _onPaste: function(e) {
+                var items = e.clipboardData && e.clipboardData.items,
+                    data = { files: [] };
+                if (items && items.length) {
+                    each(items, function (item, index) {
+                        var file = item.getAsFile && item.getAsFile();
+                        file && data.files.push(file);
+                    });
+                    if (this.options.onPaste.call(this, e, data) === false ||
+                            this._onAdd(e, data) === false) {
+                        return false;
+                    }
+                }
+            },
+
+            _onChange: function() {
+
+            },
+
+            _onAdd: function(e, data) {
+
+            },
+
+            _getTotal: function(files) {
+                var total = 0;
+                each(files, function (file) {
+                    total += file.size || 1;
+                });
+                return total;
+            },
+
+            _isXHRUpload: function(options) {
+                return (!options.multipart && support.xhrFileUpload) ||
+                    support.xhrFormDataFileUpload;
+            },
+
+            _initProgressObject: function(obj) {
+                var progress = {
+                    loaded: 0,
+                    total: 0,
+                    bitrate: 0
+                };
+                if (obj._progress) {
+                    mix(obj._progress, progress);
+                } else {
+                    obj._progress = progress;
+                }
+            },
+
+
+
+        });
+
+        return Upload;
+    }();
+
+    
+
     mix(JSFile, {
 
         mix: mix,
+
+        noop: noop,
 
         type: getType,
 
         getUid: getUid,
 
-        event: event,
+        Event: Event,
 
         BlobBuilder: BlobBuilder,
 
         Blob: Blob,
 
-        saveAs: saveAs
+        saveAs: saveAs,
 
+        upload: function(options) {
+            options || (options = {});
+            return new Upload(options);
+        },
+
+        saveAsTxt: function(text, name, charset) {
+            return saveAs(
+                new Blob([text], {
+                    type: 'text/plain;charset=' + (charset || doc.characterSet)
+                }),
+                name + '.txt');
+        },
+
+        saveAsHTML: function(html, name, charset) {
+            var _doc = createHTML(html);
+
+            return saveAs(
+                new Blob([(new XMLSerializer).serializeToString(_doc)], {
+                    type: 'application/xhtml+xml;charset=' + (charset || doc.characterSet)
+                }),
+                name + '.html');
+        }
     });
 
     win.JSFile = JSFile;
+
+    function createHTML(html) {
+        // http://www.php100.com/manual/w3school/xmldom/dom_domimplementation.asp.html
+        var im = doc.implementation,
+            _doc = im.createDocument(null, 'html', im.createDocumentType('html', null, null)),
+            _docEle = _doc.documentElement,
+            _head = _docEle.appendChild(_doc.createElement('head')),
+            _charsetMeta = head.appendChild(doc.createElement('meta')),
+            _title = head.appendChild(doc.createElement('title')),
+            _body = doc_el.appendChild(doc.createElement('body'));
+        
+        _charsetMeta.setAttribute('charset', html.ownerDocument.charset);
+        _title.appendChild(_doc.createTextNode('index'));
+        _body.innerHTML = html;
+
+        return _doc;
+    }
 
 })(this, document)
