e2c2ec773981284dcb7d823c3745365ca9ed7a50
dolymood <dolymood@gmail.com>
2013-08-11 13:02:46 +0800

    c

--- server/app.js
+++ server/app.js
@@ -5,7 +5,6 @@
 
 var express = require('express')
   , routes = require('./routes')
-  , user = require('./routes/user')
   , upload = require('./routes/upload')
   , http = require('http')
   , path = require('path');
@@ -29,8 +28,6 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-app.get('/users', user.list);
-app.get('/upload', upload.upload);
 app.post('/upload', upload.upload);
 
 http.createServer(app).listen(app.get('port'), function(){
